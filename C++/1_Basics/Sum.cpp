04783e05fc630e1b0e8a11ca8ca7995b541237d7
Cecil Git <cecilbaraik19@gmail.com>
2025-10-23 21:31:48 +0530

    First Commit

---
+++ C++/1_Basics/Sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+
+    c = a + b;
+
+    cout << "Sum of two numbers: " << c << endl;
+
+    return 0;
+}
